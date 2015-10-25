ecc28d19f2fa22567210611e7bd176158bd3ebe6
esdee-git <m@mail.com>
2015-10-25 14:50:44 +0100

    delicious has a known problem when adding links; to override it, after adding a link check if it was really added

--- posts.go
+++ posts.go
@@ -3,14 +3,17 @@
 package main
 
 import (
+	"crypto/tls"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"net/url"
+	"strings"
 )
 
-const posts_url = "https://api.del.icio.us/v1/posts/add?"
+const addPostsUrl = "https://api.del.icio.us/v1/posts/add?"
+const getPostsUrl = "https://api.del.icio.us/v1/posts/get?"
 
 func AddLink(c *Credentials, urll, tags, description string) string {
 	log.Println("AddLink called")
@@ -19,8 +22,55 @@ func AddLink(c *Credentials, urll, tags, description string) string {
 	values.Set("description", description)
 	values.Set("tags", tags)
 
-	client := &http.Client{}
-	req, _ := http.NewRequest("GET", posts_url+values.Encode(), nil)
+	client := &http.Client{Transport: &http.Transport{
+		TLSClientConfig: &tls.Config{
+			InsecureSkipVerify: true,
+		},
+	}}
+
+	req, _ := http.NewRequest("GET", addPostsUrl+values.Encode(), nil)
+	req.Header.Set("Authorization", "Bearer "+c.AccessToken)
+
+	res, err := client.Do(req)
+	if err != nil {
+		return err.Error()
+	}
+
+	if res.StatusCode != 200 {
+		msg := fmt.Sprintf("Auth failed. Response %s.", res.Status)
+		return msg
+	}
+
+	defer res.Body.Close()
+
+	body, err := ioutil.ReadAll(res.Body)
+
+	bodyString := string(body)
+	//delicious api currently has a flaw in returning error even when link was added
+	//need to check if this is the case
+	if strings.Contains(bodyString, "error adding link") {
+		if strings.Contains(GetPost(c, urll), "no bookmarks") {
+			return string(body)
+		} else {
+			return "<?xml version=\"1.0\" encoding=\"UTF-8\"?><result code=\"done\"/>"
+		}
+	}
+
+	return string(body)
+}
+
+func GetPost(c *Credentials, urll string) string {
+	log.Println("GetPost called")
+	values := url.Values{}
+	values.Set("url", urll)
+
+	client := &http.Client{Transport: &http.Transport{
+		TLSClientConfig: &tls.Config{
+			InsecureSkipVerify: true,
+		},
+	}}
+
+	req, _ := http.NewRequest("GET", getPostsUrl+values.Encode(), nil)
 	req.Header.Set("Authorization", "Bearer "+c.AccessToken)
 
 	res, err := client.Do(req)
